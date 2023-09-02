4f9a0398795f930227a66d5deb742eaa9af5d8c0
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-09-02 17:09:42 -0300

    timeout de 5

---
+++ src/api/api.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class API():
+
+    @classmethod
+    def ober_dados_linha(cls, codigo_linha: str):
+        url = 'https://sistemas.sptrans.com.br/PlanOperWeb/detalheLinha.asp'
+        params = {
+            'TpDiaID': '0',
+            'project': 'OV',
+            'lincod': codigo_linha
+        }
+        res = requests.get(url, params=params, timeout=5)
+        html_page = res.text
+        principal = BeautifulSoup(html_page, 'html.parser')
+        area_codigo = principal.find(id='areCod').attrs['value']
+        consocio = principal.find(id='consorcio').attrs['value']
+        empresa = principal.find(id='empresa').attrs['value']
+        return codigo_linha, area_codigo, consocio, empresa
