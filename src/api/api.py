09e5bf1e2e2f890558891728eea3afb12f228e59
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-09-02 20:13:17 -0300

    usando as variáveis do apache airflow

--- src/api/api.py
+++ src/api/api.py
@@ -1,6 +1,7 @@
 from typing import Tuple
 import requests
 from bs4 import BeautifulSoup
+from airflow.models import Variable
 
 
 class API():
@@ -21,7 +22,7 @@ class API():
             'project': 'OV',
             'lincod': codigo_linha
         }
-        res = requests.get(url, params=params, timeout=10)
+        res = requests.get(url, params=params, timeout=20)
         html_page = res.text
         principal = BeautifulSoup(html_page, 'html.parser')
         area_codigo = principal.find(id='areCod').attrs['value']
@@ -29,7 +30,27 @@ class API():
         empresa = principal.find(id='empresa').attrs['value']
         return codigo_linha, area_codigo, consocio, empresa
 
+    @classmethod
+    def fazer_login_api(cls) -> Tuple[str, bool]:
+        try:
+            auth = requests.post(
+                Variable.get('URL_SPTRANS') + '/v2.1/Login/Autenticar?token=' + Variable.get('TOKEN_SPTRANS'))
+            credenciais = auth.headers["Set-Cookie"].split(";")[
+                0].split("=")[-1]
+            return credenciais, True
+        except requests.exceptions.ConnectTimeout:
+            return 'Timeout no login', False
+        except requests.exceptions.ConnectionError:
+            return 'Erro na conexão', False
+
+    @classmethod
+    def pos(cls):
+        api_chave = cls.fazer_login_api()
+        pos = requests.get(
+            'http://api.olhovivo.sptrans.com.br/v2.1/Posicao', cookies={"apiCredentials": api_chave[0]}
+        )
+        return pos.json()
+
 
 if __name__ == '__main__':
-    a = API.ober_dados_linha('1012-10')
-    print(a)
+    print(API.pos())
