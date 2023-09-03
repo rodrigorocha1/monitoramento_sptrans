b7f8035dca0c7f6a542208e928f264633023bea6
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-09-02 23:04:32 -0300

    execute

--- operators/sptrans_operator.py
+++ operators/sptrans_operator.py
@@ -1,6 +1,18 @@
+from typing import Any
 from airflow.models import BaseOperator
+from airflow.utils.context import Context
+try:
+    import sys
+    import os
+    sys.path.insert(0, os.path.abspath(os.curdir))
+except ModuleNotFoundError:
+    pass
+from hook.sptrans_hook import SptransHook
 
 
 class SptransOperator(BaseOperator):
     def __init__(self,  **kwargs):
         super().__init__(**kwargs)
+
+    def execute(self, context: Context) -> Any:
+        req = SptransHook().run()
