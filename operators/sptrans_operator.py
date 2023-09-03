1cd63e636787a0e5fababdd47d8bde443b93f352
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-09-02 23:13:45 -0300

    finalização do método execute

--- operators/sptrans_operator.py
+++ operators/sptrans_operator.py
@@ -1,4 +1,5 @@
 from typing import Any
+import json
 from airflow.models import BaseOperator
 from airflow.utils.context import Context
 try:
@@ -11,8 +12,25 @@ from hook.sptrans_hook import SptransHook
 
 
 class SptransOperator(BaseOperator):
+    """_summary_
+
+    Args:
+        BaseOperator (_type_): _description_
+    """
+
     def __init__(self,  **kwargs):
         super().__init__(**kwargs)
 
     def execute(self, context: Context) -> Any:
+        """_summary_
+
+        Args:
+            context (Context): _description_
+
+        Returns:
+            Any: _description_
+        """
         req = SptransHook().run()
+        with open('extracao_operacao.json', 'w') as output_file:
+            json.dump(req, output_file, ensure_ascii=False)
+            output_file.write('\n')
